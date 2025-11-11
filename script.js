8215ad8f080bef461fa13db5ae7facb8cb7c36d8
sasa <Jihansyatsalsabila15@gmail.com>
2025-11-11 19:25:27 +0700

    Update tampilan produk, developer, dan style

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-script.js
+// MENU RESPONSIVE
+const menuToggle = document.getElementById('menu-toggle');
+const navList = document.getElementById('nav-list');
+
+menuToggle.addEventListener('click', () => {
+  navList.classList.toggle('show');
+});
+
+// TOMBOL BELI PRODUK
+function beli(nama) {
+  alert("Pesanan: " + nama + "\nSilakan hubungi admin WhatsApp ✅");
+}
