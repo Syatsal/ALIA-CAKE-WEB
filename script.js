b9818a6f90c78bdf6ae028888be9a69ad648542a
sasa <Jihansyatsalsabila15@gmail.com>
2025-11-12 14:12:55 +0700

    Update contact dan style

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const menuToggle = document.getElementById('menu-toggle');
-const navList = document.getElementById('nav-list');
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menu-toggle");
+  const navList = document.getElementById("nav-list");
 
-menuToggle.addEventListener('click', () => {
-  navList.classList.toggle('show');
-  menuToggle.classList.toggle('open');
+  menuToggle.addEventListener("click", function () {
+    navList.classList.toggle("show");
+    menuToggle.classList.toggle("open");
+  });
 });
-
-
   function beli(nama) {
     alert("Pesanan: " + nama + "\nSilakan hubungi admin WhatsApp ✅");
   }
